f680d68267380b34292ff7b3020823463742a465
Saumya Agarwal <saumyaagarwal2011@gmail.com>
2026-03-31 18:36:26 -0400

    make it streaming

--- convolve.sv
+++ convolve.sv
@@ -1,11 +1,9 @@
 module convolve #(parameter ACTIVATION_DIMENSIONS = 4) (
-    input logic [31:0] weight_in [9],
-    input logic [31:0] activation,
+    input logic [31:0] input_value_from_processor,
     input logic clk,
     input logic rst_n,
     input logic start,
-    output logic [31:0] result,
-    output logic finished
+    output logic [31:0] result
 );
 
     logic[31:0] weight[9];
@@ -13,8 +11,8 @@ module convolve #(parameter ACTIVATION_DIMENSIONS = 4) (
     logic[31:0] outputs_b[3];
     logic[31:0] outputs_c[3];
     logic[31:0] shift_temp[3];
-
-    logic [ACTIVATION_DIMENSIONS-1:0] counter;
+    logic state;
+    logic [3:0] counter;
     genvar i;
     genvar j;
     genvar a;
@@ -22,27 +20,30 @@ generate
     for (i = 0; i < 3; i++) begin
         mac mac_written_1 (
             .weight(weight[(i*3)]),
-            .activation(activation),
+            .activation(input_value_from_processor),
             .previous(shift_temp[i]),
             .output_val(outputs_a[i]),
             .clk(clk),
-            .rst_n(rst_n)
+            .rst_n(rst_n),
+            .en(state)
         );
         mac mac_written_2 (
             .weight(weight[(i*3)+1]),
-            .activation(activation),
+            .activation(input_value_from_processor),
             .previous(outputs_a[i]),
             .output_val(outputs_b[i]),
             .clk(clk),
-            .rst_n(rst_n)
+            .rst_n(rst_n),
+            .en(state)
         );
         mac mac_written_3 (
             .weight(weight[(i*3)+2]),
-            .activation(activation),
+            .activation(input_value_from_processor),
             .previous(outputs_b[i]),
             .output_val(outputs_c[i]),
             .clk(clk),
-            .rst_n(rst_n)
+            .rst_n(rst_n),
+            .en(state)
         );
     end
 
@@ -63,15 +64,22 @@ endgenerate
             shift_temp[0] <= '0;
             counter <= '0;
         end else begin
+            shift_temp[0] <= '0;
             if (start) begin
-                weight <= weight_in;
                 counter <= '0;
-                finished <= '0;
-            end
-            result <= outputs_c[2];
-            counter <= counter + 1;
-            if (counter == ACTIVATION_DIMENSIONS*ACTIVATION_DIMENSIONS) begin
-                finished <= 1;
+                state <= '0;
+                result <= '0;
+            end else begin
+                if (!state) begin
+                    weight[counter] <= input_value_from_processor;
+                    counter <= counter + 1;
+                    result <= '0;
+                    if (counter == 4'h8) begin
+                        state <= 1;
+                    end
+                end else begin
+                    result <= outputs_c[2];
+                end
             end
         end
     end
--- mac.sv
+++ mac.sv
@@ -4,14 +4,19 @@ module mac(
     input logic [31:0] previous,
     output logic [31:0] output_val,
     input logic clk,
-    input logic rst_n
+    input logic rst_n,
+    input logic en
 );
 
     always_ff @( posedge clk, negedge rst_n ) begin
         if (!rst_n) begin
             output_val <= '0;
         end else begin
-            output_val <= (weight * activation) + previous;
+            if (en) begin
+                output_val <= (weight * activation) + previous;
+            end else begin
+                output_val <= 0;
+            end
         end
     end
 
